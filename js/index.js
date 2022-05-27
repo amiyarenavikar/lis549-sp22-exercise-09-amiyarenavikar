d1823bb209df48bc597da6555fd4287ec3f6ad28
Amiya Renavikar <amiya@uw.edu>
2022-05-27 09:14:58 -0700

    finished part 12

--- js/index.js
+++ js/index.js
@@ -65,7 +65,8 @@ $('img').css({'cursor': 'pointer'});
 
 
 //Add the `data-bs-toggle` and `data-bs-target` attributes to the images
-
+$('img').attr('data-bs-toggle', 'modal');
+$('img').attr('data-bs-target', '#niceModal');
 
 //Ad an event listener to the modal for `show.bs.modal` events.
 //The listener's function will replace the modal's image `src` and
